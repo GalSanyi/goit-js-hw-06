643156fb7a16f6980b4f454d1718f6e4828a7914
GalSanyi <galsanyi84@gmail.com>
2021-11-12 21:30:04 +0100

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,42 @@
+const refs = {
+    loginForm: document.querySelector(".login-form"),
+    email: document.querySelector(".login-form input[name='email']"),
+    password: document.querySelector(".login-form input[name='password']"),
+    button: document.querySelector(".login-form button"),
+};
 
+refs.loginForm.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+
+    if (!checkField(refs)) return;
+
+
+    // ---------------------------------------------------------
+    const resObject = new FormData(event.currentTarget);
+    // Вывод объекта в консоль
+    resObject.forEach((value, name) => {
+        console.log(`field ${name} = ${value}`)
+    });
+    // ---------------------------------------------------------
+
+
+    refs.loginForm.reset();
+}
+
+function checkField({ email, password }) {
+    if (email.value === "") {
+        alert("Email field is empty");
+        return false;
+    }
+    if (password.value === "") {
+        alert(" Password field is empty");
+        return false;
+    }
+    return true;
+}
+new Map();
