3e17577417f7a355e9995a708a47e6c4584a435d
GalSanyi <galsanyi84@gmail.com>
2021-11-13 16:27:39 +0100

    task -9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+ const body = document.querySelector('body');
+ const Background = document.querySelector('.color');
+ const button = document.querySelector('.change-color');
+
+
+
+ function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+ button.addEventListener('click', onGenerator);
+
+ const color = getRandomHexColor();
+
+ function onGenerator() {
+     getRandomHexColor();
+     body.style.background = color;
+     button.style.color = color;
+     Background.textContent = color;
+
+ };
